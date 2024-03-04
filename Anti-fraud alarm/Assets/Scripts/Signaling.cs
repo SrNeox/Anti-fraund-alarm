936cdc0b9a1eee41a2df76e4e99f43d04b300838
SrNeox <gsabinskij33@gmail.com>
2024-03-04 23:35:16 +0300

    Refand

--- Anti-fraud alarm/Assets/Scripts/Signaling.cs
+++ Anti-fraud alarm/Assets/Scripts/Signaling.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Signaling : MonoBehaviour
@@ -7,38 +7,30 @@ public class Signaling : MonoBehaviour
     [SerializeField] private AudioSource _audioSourse;
 
     private bool _playerInside;
-    private float _maxVolume = 1;
-    private float _minVolum = 0;
-    private float _increaseRate = 0.1f;
-    private float _decreaseRate = 0.1f;
 
-    private void OnTriggerEnter(Collider other)
+    public void Launch(bool playerInside)
     {
-        _playerInside = true;
-        StartCoroutine(IncreaseVolume());
+        _playerInside = playerInside;
+        StartCoroutine(ChangeVolume());
     }
 
-    private void OnTriggerExit(Collider other)
+    private IEnumerator ChangeVolume()
     {
-        _playerInside = false;
-        StartCoroutine(DecreaseVolume());
-    }
+        float maxVolume = 1;
+        float minVolum = 0;
+        float increaseRate = 0.1f;
+        float decreaseRate = 0.1f;
 
-    private IEnumerator IncreaseVolume()
-    {
-        while (_playerInside == true)
+        while (true)
         {
-            _audioSourse.volume = Mathf.MoveTowards(_audioSourse.volume, _maxVolume, _increaseRate * Time.deltaTime);
-
-            yield return null;
-        }
-    }
-
-    private IEnumerator DecreaseVolume()
-    {
-        while (_playerInside == false)
-        {
-            _audioSourse.volume = Mathf.MoveTowards(_audioSourse.volume, _minVolum, _decreaseRate * Time.deltaTime);
+            if (_playerInside == true)
+            {
+                _audioSourse.volume = Mathf.MoveTowards(_audioSourse.volume, maxVolume, increaseRate * Time.deltaTime);
+            }
+            else
+            {
+                _audioSourse.volume = Mathf.MoveTowards(_audioSourse.volume, minVolum, decreaseRate * Time.deltaTime);
+            }
 
             yield return null;
         }
---
+++ Anti-fraud alarm/Assets/Scripts/SoundTriger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+
+public class SoundTriger : MonoBehaviour
+{
+    [SerializeField] private Signaling _signaling;
+
+    private bool _playerInside;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _playerInside = true;
+        _signaling.Launch(_playerInside);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _playerInside = false;
+        _signaling.Launch(_playerInside);
+    }
+}
